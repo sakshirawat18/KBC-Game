2b86b91a2662a18ca415e63eec857dada119e7f4
Sakshi Rawat <sakshirawat@Sakshis-Air.Optilink>
2023-03-04 14:22:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,162 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useMemo, useState } from "react";
+import Start from "./Components/Start";
+import Timer from "./Components/Timer";
+import Quiz from "./Components/Quiz";
 
 function App() {
+  const [username, setUsername] = useState(null);
+  const [timeOut, setTimeOut] = useState(false);
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [earned, setEarned] = useState("0");
+
+  const data = [
+    {
+      id: 1,
+      question: "Which of the following corresponds to (ek bataa do)?",
+      answers: [
+        {
+          text: "Pura",
+          correct: false,
+        },
+        {
+          text: "Adha",
+          correct: true,
+        },
+        {
+          text: "Sawa",
+          correct: false,
+        },
+        {
+          text: "Pauna",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question:
+        "In the game of ludo the discs or tokens are of how many colours?",
+      answers: [
+        {
+          text: "one",
+          correct: false,
+        },
+        {
+          text: "two",
+          correct: false,
+        },
+        {
+          text: "three",
+          correct: false,
+        },
+        {
+          text: "four",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Where was the BRICS summit held in 2022?",
+      answers: [
+        {
+          text: "India",
+          correct: false,
+        },
+        {
+          text: "Brazil",
+          correct: false,
+        },
+        {
+          text: "Russia",
+          correct: false,
+        },
+        {
+          text: "China",
+          correct: true,
+        },
+      ],
+    },
+  ];
+
+  const moneyPyramid = useMemo(
+    () =>
+      [
+        { id: 1, amount: "1,000" },
+        { id: 2, amount: "2,000" },
+        { id: 3, amount: "3,000" },
+        { id: 4, amount: "5,000" },
+        { id: 5, amount: "10,000" },
+        { id: 6, amount: "20,000" },
+        { id: 7, amount: "40,000" },
+        { id: 8, amount: "80,000" },
+        { id: 9, amount: "1,60,000" },
+        { id: 10, amount: "3,20,000" },
+        { id: 11, amount: "6,40,000" },
+        { id: 12, amount: "12,50,000" },
+        { id: 13, amount: "25,00,000" },
+        { id: 14, amount: "50,00,000" },
+        { id: 15, amount: "1 Crore" },
+        { id: 16, amount: "7 Crore" },
+      ].reverse(),
+    []
+  );
+
+  useEffect(() => {
+    questionNumber > 1 &&
+      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [questionNumber, moneyPyramid]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!username ? (
+        <Start setUsername={setUsername} />
+      ) : (
+        <>
+          <div className="main">
+            {timeOut ? (
+              <h1 className="endText">You earned: {earned}</h1>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer
+                      setTimeOut={setTimeOut}
+                      questionNumber={questionNumber}
+                    />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Quiz
+                    data={data}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                    setTimeOut={setTimeOut}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <div className="pyramid">
+            <ul className="moneyList">
+              {moneyPyramid.map((m) => (
+                <li
+                  key={m.id}
+                  className={
+                    questionNumber === m.id
+                      ? "moneyListItem active"
+                      : "moneyListItem"
+                  }
+                >
+                  <span className="moneyListItemNumber">{m.id}</span>
+                  <span className="moneyListItemAmount">{m.amount}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/Components/Quiz.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import play from "../sounds/play.mp3";
+import correct from "../sounds/correct.mp3";
+import wrong from "../sounds/wrong.mp3";
+
+export default function Quiz({
+  data,
+  questionNumber,
+  setQuestionNumber,
+  setTimeOut,
+}) {
+  const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
+  const [letsPlay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
+
+  useEffect(() => {
+    letsPlay();
+  }, [letsPlay]);
+
+  useEffect(() => {
+    setQuestion(data[questionNumber - 1]);
+  }, [data, questionNumber]);
+
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback();
+    }, duration);
+  };
+
+  const handleClick = (a) => {
+    setSelectedAnswer(a);
+    setClassName("answer active");
+    delay(3000, () => {
+      setClassName(a.correct ? "answer correct" : "answer wrong");
+    });
+
+    delay(5000, () => {
+      if (a.correct) {
+        correctAnswer();
+        delay(1000, () => {
+          setQuestionNumber((prev) => prev + 1);
+          setSelectedAnswer(null);
+        });
+      } else {
+        wrongAnswer();
+        delay(1000, () => {
+          setTimeOut(true);
+        });
+      }
+    });
+  };
+  return (
+    <div className="quiz">
+      <div className="question">{question?.question}</div>
+      <div className="answers">
+        {question?.answers.map((a) => (
+          <div
+            key={a.text}
+            className={selectedAnswer === a ? className : "answer"}
+            onClick={() => !selectedAnswer && handleClick(a)}
+          >
+            {a.text}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
